369389242c1d2b40526f4f189f277d2deb083593
Saniha123308 <ckcc0801@leeds.ac.uk>
2026-02-24 18:59:15 +0000

    portfolio

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,17 +1,33 @@
 
 /*
-Name:
-Student ID:
+Name: Saniha Srikanth Kakolu
+Student ID: 201964405
 */
-
 #include <stdio.h>
 #include <string.h>
 
 int main( int argc, char **argv ) {
-
+    char buffer[100];
+    
+   
     // define appropriate data to hold your answer
-
-    // process the command-line data using appropriate string functions
+    if (argc < 2 || argc > 11) {  
+        return 1; 
+    } 
+    // 2. Check each argument length (1–4 chars) 
+    for (int i = 1; i < argc; i++) { 
+        int len = strlen(argv[i]); 
+        if (len < 1 || len > 5) { 
+             return 1;
+            } 
+    }
+    buffer[0]='\0';
+    for (int i = 1; i < argc; i++){
+        strcat(buffer, argv[i]);
+        if (i < argc - 1) {
+            strcat(buffer, "-");
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
